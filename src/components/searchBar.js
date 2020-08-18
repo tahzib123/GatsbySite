85dc1dbe2606c67a596630db6db0665231c43e9a
Tahzib Sunesara <tahzibsunesara1@gmail.com>
2020-08-18 12:30:12 -0500

    fixed search bug

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -6,7 +6,7 @@ import searchBarStyles from "../styles/searchBar.module.css";
 const searchBar = ({handleChange, handleSubmit}) => {
     return(
         <div className = {searchBarStyles.formContainer}>
-            <form className = {searchBarStyles.searchForm}  onSubmit = {handleSubmit}>
+            <form id = "searchForm" className = {searchBarStyles.searchForm}  onSubmit = {handleSubmit}>
                 {/* eslint-disable-next-line  */}
                 <input className = {searchBarStyles.searchBox} type = "text" placeholder = "Search for a Product" onChange = {handleChange} />
                 <button className = {searchBarStyles.submitButton} type = "submit" >Go</button>
